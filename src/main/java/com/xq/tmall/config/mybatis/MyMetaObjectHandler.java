d74fa3b5db611308cbb0ab5ada0e37a1244f62f8
haitao <18920079569@163.com>
2023-02-02 14:42:44 +0800

    补充

--- src/main/java/com/xq/tmall/config/mybatis/MyMetaObjectHandler.java
+++ src/main/java/com/xq/tmall/config/mybatis/MyMetaObjectHandler.java
@@ -15,11 +15,6 @@ public class MyMetaObjectHandler extends MetaObjectHandler {
      */
 	 @Override
     public void insertFill(MetaObject metaObject) {
-        // 更多查看源码测试用例
-        System.out.println("*************************");
-        System.out.println("insert fill");
-        System.out.println("*************************");
-
         // 测试下划线
         Object testType = getFieldValByName("gmtCreate", metaObject);//mybatis-plus版本2.0.9+
         System.out.println("gmtCreate=" + testType);
@@ -30,10 +25,6 @@ public class MyMetaObjectHandler extends MetaObjectHandler {
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        //更新填充
-        System.out.println("*************************");
-        System.out.println("update fill");
-        System.out.println("*************************");
         //mybatis-plus版本2.0.9+
         setFieldValByName("gmtModified", new Timestamp(System.currentTimeMillis()), metaObject);
     }
