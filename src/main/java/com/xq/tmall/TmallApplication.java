f3642e2fe3322e60e67e2742d8a4627c1ac11301
haitao <18920079569@163.com>
2024-06-03 13:40:27 +0800

    补充

--- src/main/java/com/xq/tmall/TmallApplication.java
+++ src/main/java/com/xq/tmall/TmallApplication.java
@@ -2,8 +2,6 @@ package com.xq.tmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
